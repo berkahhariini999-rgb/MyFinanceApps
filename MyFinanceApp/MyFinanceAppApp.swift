e8f460c301c901d8926c616969a97ac76eb8c203
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-10-10 18:50:30 +0700

    Initial Commit

---
+++ MyFinanceApp/MyFinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceAppApp.swift
+//  MyFinanceApp
+//
+//  Created by Iqbal Alhadad on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
